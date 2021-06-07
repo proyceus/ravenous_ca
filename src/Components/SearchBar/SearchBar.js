da628d6b8fce49bb74565affb7bd6eae19ef4690
Pierre Royce <proyceus@gmail.com>
2021-06-07 15:27:50 -0400

    Adding in components

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchBar.css';
+
+let sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
+
+export default class SearchBar extends React.Component {
+  renderSortByOptions() {
+
+  }
+}

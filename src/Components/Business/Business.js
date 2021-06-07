c3e29625515f2436ba20474bae4fbbc244d84d32
Pierre Royce <proyceus@gmail.com>
2021-06-07 17:24:06 -0400

    Minor updates

--- src/Components/Business/Business.js
+++ src/Components/Business/Business.js
@@ -1,19 +1,7 @@
 import React from "react";
 import './Business.css';
 
-let business = {
-  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Flavortown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
-
-export default class Business extends React.Component {
+class Business extends React.Component {
   render() {
     return (
       <div className="Business">
@@ -23,20 +11,22 @@ export default class Business extends React.Component {
             alt=""
           />
         </div>
-        <h2>{business.name}</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{business.state}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating} stars}</h3>
-            <p>{business.reviewCount} reviews</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating} stars}</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
     );
   }
 }
+
+export default Business;
